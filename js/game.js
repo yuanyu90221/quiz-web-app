f8db825e3f27e4d56c32324a0ec7640cf098089c
yuanyu90221 <yuanyu90221@gmail.com>
2019-03-20 01:21:11 +0800

    add score and question numbers

--- js/game.js
+++ js/game.js
@@ -1,5 +1,7 @@
 const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName('choice-text'));
+const questionCounterText = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -52,6 +54,7 @@ getNewQuestion = () => {
     return window.location.assign('/end.html');
   }
   questionCounter++;
+  questionCounterText.innerText = `${questionCounter}/${MAX_QUESTIONS}`; 
   const questionIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
@@ -73,6 +76,9 @@ choices.forEach(choice => {
     const selectedChoice = e.target;
     const selectedAnswer = selectedChoice.dataset['number'];
     const classToApply = (selectedAnswer == currentQuestion.answer)? 'correct':'incorrect';
+    if( classToApply === 'correct') {
+      incrementScore(CORRECT_BONUS);
+    }
     selectedChoice.parentElement.classList.add(classToApply);
     setTimeout(()=>{
       selectedChoice.parentElement.classList.remove(classToApply);
@@ -80,4 +86,9 @@ choices.forEach(choice => {
     }, 1000);
   });
 });
+
+incrementScore = num => {
+  score += num;
+  scoreText.innerText = score;
+};
 startGame();
