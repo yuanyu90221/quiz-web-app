8717bf5a1244bd3606575eee88ed0ad0b89492b3
yuanyu90221 <yuanyu90221@gmail.com>
2019-03-24 02:12:36 +0800

    add animation

--- js/game.js
+++ js/game.js
@@ -3,7 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const scoreText = document.getElementById('score');
 const progressBarFull = document.getElementById('progressBarFull');
-
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 let currentQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -31,19 +32,22 @@ fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=medium&type=m
       });
       return formattedQuestion;
     });
+   
     startGame();
   }).catch(err=>{
     console.error(err);
   });
 //CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 5;
 
 startGame = () => {
   questionCounter = 0;
   score = 0;
   availableQuestions = [...questions];
   getNewQuestion();
+  game.classList.remove('hidden');
+  loader.classList.add('hidden');
 }
 getNewQuestion = () => {
   if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
@@ -58,10 +62,9 @@ getNewQuestion = () => {
   const questionIndex = Math.floor(Math.random() * availableQuestions.length)
   currentQuestion = availableQuestions[questionIndex];
   question.innerText = currentQuestion.question;
-
   choices.forEach( choice => {
     const number = choice.dataset['number'];
-    choice.innerText = currentQuestion['choice' + number];
+    choice.innerHTML = currentQuestion['choice' + number];
   });
 
   availableQuestions.splice(questionIndex, 1);
